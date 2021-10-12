7598942849f20a319f89aa19f0bfb46211091079
Keewon Shin <kevinkwshin@gmail.com>
2021-10-13 08:04:33 +0900

    Update heads.py

--- segmentation_models_pytorch/base/heads.py
+++ segmentation_models_pytorch/base/heads.py
@@ -22,3 +22,11 @@ class ClassificationHead(nn.Sequential):
         linear = nn.Linear(in_channels, classes, bias=True)
         activation = Activation(activation)
         super().__init__(pool, flatten, dropout, linear, activation)
+
+class ReconstructionHead(nn.Sequential):
+    
+    def __init__(self, in_channels, out_channels, kernel_size=3, activation=None, upsampling=1):
+        conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2)
+        upsampling = nn.UpsamplingBilinear2d(scale_factor=upsampling) if upsampling > 1 else nn.Identity()
+        activation = Activation(activation)
+        super().__init__(conv2d, upsampling, activation)
